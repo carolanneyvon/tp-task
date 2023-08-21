74306300e815ba8b17b8db6a39778c80375c9b1e
Carolanne Yvon <elolithia@gmail.com>
2023-08-21 14:16:12 +0200

    Si done = true alors la tâches passe en dernière

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -27,11 +27,20 @@ function App() {
 
   setTasks(updatedTasks);
 }
+
+  // Tri des tâches pour afficher les tâches non terminées en premier
+  // sort compare chaque chaque élément du tableau les uns avec les autres
+  const sortedTasks = [...tasks].sort((a, b) => {
+    if (a.done === b.done) {
+      return 0; // L'ordre reste inchangé
+    }
+    return a.done ? 1 : -1; // si =1 la tâche a sera après la b
+  });
   
   return (
     <div className="App container">
-      <h1>Liste des tâches</h1>
-      {tasks.map((task) => <Task key={task.id} {...task} onClickValidate={ handleClickValidate } />)}
+      <h1 className='text-center'>Liste des tâches</h1>
+      {sortedTasks.map((task) => <Task key={task.id} {...task} onClickValidate={ handleClickValidate } />)}
     </div>
   );
 }
